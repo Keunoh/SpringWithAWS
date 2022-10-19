ec0aa9a52382b19163ba30267a229c88348869b8
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-19 19:24:50 +0900

    ensures transactional posts service layer

---
+++ src/main/java/com/keunoh/book/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.keunoh.book.springboot.service;
+
+import com.keunoh.book.springboot.domain.posts.PostsRepository;
+import com.keunoh.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
