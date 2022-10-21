5a588725bc4488f639c2b45cd5bf8491af324871
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-21 14:49:07 +0900

    update index method for social login with SessionUser information

--- src/main/java/com/keunoh/book/springboot/web/IndexController.java
+++ src/main/java/com/keunoh/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.keunoh.book.springboot.web;
 
+import com.keunoh.book.springboot.config.auth.dto.SessionUser;
 import com.keunoh.book.springboot.service.PostsService;
 import com.keunoh.book.springboot.web.dto.PostsResponseDto;
 import lombok.Getter;
@@ -9,15 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
@@ -33,5 +43,6 @@ public class IndexController {
 
         return "posts-update";
     }
+
 
 }
